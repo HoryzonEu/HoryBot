4b0f17f05741cd92fee8784af688508b0411f4b3
Azod <azod.contact@gmail.com>
2020-11-03 01:33:44 +0100

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+'use strict';
+
+const fs = require("fs");
+const { token } = require("./config.js");
+
+bot.on("ready", function (){
+    bot.user.setActivity("Pour vous servir !");
+    console.log("HoryBot is ready !")
+});
+
+bot.login(token);
