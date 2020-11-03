0a7099c31a1a99d0e94299c9a93fcef6f6407ada
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-11-03 04:12:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.once("ready", function (){
    
     //Auto role message
     const auto_role_message = channel.messages.cache.first();
-    console.log(`${guild.name} -- ${channel.name} -- ${guild.channels.cache.get("773002258316394496").messages.cache.first()}`);
+    console.log(`${guild.name} -- ${channel.name} -- ${guild.channels.cache.get("773002258316394496").messages.fetch("773019180156583947").content}`);
    /* const filter = (reaction, user) => reaction.emoji.name === "1️⃣" || reaction.emoji.name === "2️⃣";
     const auto_role = auto_role_message.createReactionCollector(filter);
     auto_role.on("collect", (reaction, user) => {
