efe18645537190606bc1136c47da6703acdfc408
Azod <azod.contact@gmail.com>
2020-11-03 02:11:31 +0100

    add auto delete commands

--- cmds/Ping.js
+++ cmds/Ping.js
@@ -7,7 +7,10 @@ module.exports = {
     //Description for a potential help command
     description: "Test the bot reaction",
     //The script of the command
-    execute(message, args){
-        message.channel.send("Pong !");
+    async execute(message, args){
+        //Delete the message after 5 seconds
+        message.delete({ timeout: 5000 })
+        //Reply to the commands then delete the reply after 5 seconds
+        message.channel.send("Pong !").then(m => m.delete({ timeout: 5000 }));
     }
 }
