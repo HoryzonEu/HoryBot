74a6c1d20a504918b8bcec3bc7e569132dfc48c6
Azod <azod.contact@gmail.com>
2020-11-10 00:33:48 +0100

    generate key

--- cmds/Link.js
+++ cmds/Link.js
@@ -3,10 +3,11 @@ module.exports = {
     description: "Link account to a project",
     execute (message, args){
         if (message.channel.type != "dm"){
-            message.reply("cette commande n'es utilisable qu'en message privé !");
-            message.delete({timeout: 1000});
+            message.reply("cette commande n'est utilisable qu'en message privé !");
+            message.delete({timeout: 0});
             return;
         }
-        message.channel.send('works!')
+        const code = Date.now().slice(9);
+        message.channel.send(code)
     }
 }
